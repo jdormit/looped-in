9bf8d142c6a49b743da4b97574dfed0797dd5b2f
Jeremy Dormitzer <jdormitzer@hubspot.com>
2018-01-06 21:38:01 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to Looped In
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/looped_in/core.cljs
@@ -0,0 +1,22 @@
+(ns looped-in.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [ajax.core :refer [GET]]
+            [cljs.core.async :as async :refer [chan <! >!]]))
+
+(enable-console-print!)
+
+(defn fetch-submission
+  "Fetches submissions from Hacker News by `url`"
+  [url]
+  (let [response-chan (chan)]
+    (GET "http://hn.algolia.com/api/v1/search"
+         {:params {"query" url
+                   "restrictSearchableAttributes" "url"}
+          :handler (fn [res] (go (>! response-chan res)))
+          :error-handler (fn [err] (go (>! response-chan err)))})
+    response-chan))
+
+(let [current-url (-> js/window (.-location) (.-href))
+      sub-chan (fetch-submission current-url)]
+  (go (println (<! sub-chan))))
+
