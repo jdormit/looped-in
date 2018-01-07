cec7edea5cebe4c0512c3c80c67a158a483b201d
Jeremy Dormitzer <jdormitzer@hubspot.com>
2018-01-07 12:12:53 -0500

    Organize project.clj

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [cljs-ajax "0.7.3"]
-                 [org.clojure/core.async "0.3.465"]]
+                 [org.clojure/core.async "0.3.465"]
+                 [cljs-ajax "0.7.3"]]
   :plugins [[lein-cljsbuild "1.1.7"]]
   :cljsbuild {:builds
               [{:source-paths ["src"]
