7ab7a9889dd976e37e87b8e5f0748139d6195f92
Jeremy Dormitzer <jdormitzer@hubspot.com>
2018-01-08 10:47:50 -0500

    Output validly encoded JS

--- src/build/looped_in/build.clj
+++ src/build/looped_in/build.clj
@@ -4,9 +4,11 @@
 (defn -main [& args]
   (build "src/content" {:output-to "ext/content.js"
                         :output-dir "out"
+                        :closure-output-charset "US-ASCII"
                         :optimizations :whitespace
                         :pretty-print true})
   (build "src/background" {:output-to "ext/background.js"
                            :output-dir "out"
+                           :closure-output-charset "US-ASCII"
                            :optimizations :whitespace
                            :pretty-print true}))
