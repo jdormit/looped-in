1661086da2e09c9990f5fe512a439e6eb0ebddf3
Jeremy Dormitzer <jdormitzer@hubspot.com>
2018-01-10 08:04:10 -0500

    Fix build output reference errors

--- src/build/looped_in/build.clj
+++ src/build/looped_in/build.clj
@@ -3,12 +3,10 @@
 
 (defn -main [& args]
   (build "src/content" {:output-to "ext/content.js"
-                        :output-dir "out"
+                        :optimizations :simple
                         :closure-output-charset "US-ASCII"
-                        :optimizations :whitespace
                         :pretty-print true})
   (build "src/background" {:output-to "ext/background.js"
-                           :output-dir "out"
+                           :optimizations :simple
                            :closure-output-charset "US-ASCII"
-                           :optimizations :whitespace
                            :pretty-print true}))
