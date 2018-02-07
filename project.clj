746d9fccc99914d84b51aac812997c37fbcc9679
Jeremy Dormitzer <jdormitzer@hubspot.com>
2018-02-07 09:05:26 -0500

    Fix project.clj typo

--- project.clj
+++ project.clj
@@ -49,7 +49,7 @@
                                    [org.clojure/tools.nrepl "0.2.10"]]
                     :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}]
              :prod [:cljs-shared
-                    {:cljbuilds
+                    {:cljsbuild
                      {:builds
                       {:background
                        {:compiler {:optimizations :simple
